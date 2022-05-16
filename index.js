e9144ef620587ff67026cd42536379c53ac04c25
volterra79 <boccacci.francesco@gmail.com>
2022-05-16 16:09:18 +0200

    rollback

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const _Plugin = function() {
     const vueComponentObject = MultiPlot({
       service : this.service
     });
-    const sidebarComponent = this.createSideBarComponent(vueComponentObject,
+    this.createSideBarComponent(vueComponentObject,
       {
         id: 'qplotly',
         title: 'plugins.qplotly.title',

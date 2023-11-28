8e73f27ba6f2c53b91fecc4310e6653ce912a46a
Raruto <Raruto@users.noreply.github.com>
2023-11-28 14:46:18 +0100

    code format

--- components/sidebar/Multiplot.vue
+++ components/sidebar/Multiplot.vue
@@ -1,24 +1,31 @@
 <template>
-  <ul id="chart_plot_multi_plot" class="treeview-menu" style="padding: 10px;color:#FFFFFF">
+  <ul
+    id    = "chart_plot_multi_plot"
+    class = "treeview-menu"
+    style = "padding: 10px;color:#FFFFFF"
+  >
     <li
-      v-for="plot in plots"
-      :key="plot.id">
-
+      v-for = "plot in plots"
+      :key  = "plot.id"
+    >
       <input
-        type="checkbox"
-        :id="plot.id"
-        class="magic-checkbox"
-        v-model="plot.show"
-        @change="showHidePlot(plot)" >
-
+        type    = "checkbox"
+        :id     = "plot.id"
+        class   = "magic-checkbox"
+        v-model = "plot.show"
+        @change = "showHidePlot(plot)"
+      >
         <label
-          :class="{'g3w-disabled': loading}"
-          :for="plot.id"
-          style="display:flex; justify-content: space-between; align-items: center">
-
-            <span style="white-space: pre-wrap">{{ plot.label }} </span>
-            <span>{{plot.plot.type}}</span>
-
+          :class = "{'g3w-disabled': loading}"
+          :for   = "plot.id"
+          style  = "
+            display:flex;
+            justify-content: space-between;
+            align-items: center;
+          "
+        >
+          <span style="white-space: pre-wrap">{{ plot.label }} </span>
+          <span>{{ plot.plot.type }}</span>
         </label>
     </li>
   </ul>
@@ -29,28 +36,30 @@
 import PluginService from '../../service';
 
 export default {
+
   name: "Multiplot",
-  data(){
+
+  data() {
     return {
       plots: PluginService.getPlots(),
     };
   },
+
   computed: {
-    loading(){
+
+    loading() {
       return PluginService.state.chartsloading;
-    }
+    },
+
   },
 
-  methods:{
-    showHidePlot(plot){
-      setTimeout(()=>{
-        PluginService[plot.show && 'showPlot' || 'hidePlot'](plot);
-      })
-    }
+  methods: {
+
+    showHidePlot(plot) {
+      setTimeout(() => { PluginService[plot.show ? 'showPlot' : 'hidePlot'](plot); })
+    },
+
   },
+
 }
 </script>
-
-<style scoped>
-
-</style>

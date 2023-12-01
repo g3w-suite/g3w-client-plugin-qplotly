36bf5b64aeedd86a8492c288f6a1cda3698e34e3
Raruto <Raruto@users.noreply.github.com>
2023-12-01 08:53:43 +0100

    move `nodata.js` into `qplotly.vue`

--- components/content/qplotly.vue
+++ components/content/qplotly.vue
@@ -90,7 +90,50 @@
   const { getUniqueDomId } = g3wsdk.core.utils;
   const { resizeMixin }    = g3wsdk.gui.vue.Mixins;
 
-  const NoDataComponent    = require('./nodata');
+  const NoDataComponent    = {
+    props: {
+      title: {
+        type: String
+      }
+    },
+    render(h){
+      return h('div', {
+        style: {
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          height: '100%',
+          justifyContent: 'center'
+        }
+      }, [
+        h('h4',
+          {
+            style: {
+              fontWeight: 'bold',
+              textAlign: 'center'
+            },
+            class:{
+              'skin-color':true
+            }
+          },
+          `${this.$props.title}`
+        ),
+
+        h('div', {
+          directives: [{
+            name:'t-plugin',
+            value: 'qplotly.no_data'
+          }],
+          style: {
+            fontWeight: 'bold'
+          },
+          class: {
+            'skin-color': true
+          }
+        })
+      ])
+    }
+  };
 
   const TYPE_VALUES = {
     'pie':            'values',

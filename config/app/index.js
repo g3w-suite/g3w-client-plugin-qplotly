2113baeade5bfbb05e4a8163154daa856b71f6dd
Raruto <Raruto@users.noreply.github.com>
2024-01-16 15:22:45 +0100

    remove file: `config/app.js`

--- config/app/index.js
+++
@@ -1,7 +0,0 @@
-export const charts = {
-  no_subplots: ['pie', 'ternary']
-}
-
-export default {
-  charts
-}
--- config/index.js
+++ config/index.js
@@ -1,8 +1,7 @@
-import {name} from '../package.json';
+import { name } from '../package.json';
 import i18n from './i18n'
-import app from './app'
+
 export default {
   name,
   i18n,
-  app
 }

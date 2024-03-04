9ab7f131c0c57bcd5e7fc997b92ccb028a39b4d2
volterra79 <boccacci.francesco@gmail.com>
2024-03-04 09:50:57 +0100

    Split i18n files

--- config/i18n/index.js
+++ config/i18n/index.js
@@ -1,156 +1,17 @@
+import it from './it';
+import en from './en';
+import fi from './fi';
+import se from './se';
+import fr from './fr';
+import de from './de';
+import ro from './ro';
+
 export default {
-  it: {
-    title: 'Grafici',
-    pietitle: "Grafici Torta",
-    no_subplots_charts: 'Grafici esclusivi',
-    no_plots : 'Nessun grafico da mostrare. Aggiungi almeno un grafico',
-    no_data: "Nessun dato disponibile",
-    tooltip: {
-      show_all_features_on_map: "Filtra tutti i grafici sulla base degli elementi visibili in mappa",
-      show_feature_on_map: "Filtra questo grafico sulla base degli elementi visibili in mappa",
-      filter_chart: "Filtra i grafici sulla base degli elementi selezionati"
-    },
-    filters: {
-      in_bbox: "I dati mostrati nel grafico sono filtrati in base alle geometrie visibili sulla mappa",
-      filtertoken: "I dati mostrati nel grafico sono filtrati in base alle geometrie/records selezionati",
-      in_bbox_filtertoken: "I dati mostrati nel grafico sono filtrati in base alle geometrie/records selezionati e visibili sulla mappa",
-      relation: {
-        in_bbox: "I dati mostrati nel grafico sono filtrati in base alle geometrie di riferimento (genitori) visibili sulla mappa",
-        filtertoken: "I dati mostrati nel grafico sono filtrati in base alle geometrie/records selezionati di riferimento (genitori)",
-        in_bbox_filtertoken: "I dati mostrati nel grafico sono filtrati in base alle geometrie/records di riferimento (genitori) selezionati e visibili sulla mappa"
-      }
-    }
-  },
-  en: {
-    title: 'Charts',
-    pietitle: "Pie Charts",
-    no_subplots_charts: 'Exclusive plots',
-    no_plots : 'No chart. Add at least one plot',
-    no_data: "No Data available",
-    tooltip: {
-      show_all_features_on_map: "Filter all plots according to the geometries visible on the map",
-      show_feature_on_map: "Filter plot according to the geometries visible on the map",
-      filter_chart: "Filter plots according to the selected geometries/records"
-    },
-    filters: {
-      in_bbox: "The data ​​shown in the plot are filtered according to the geometries visible on the map",
-      filtertoken: "The data ​​shown in the plot are filtered according to the selected geometries/records",
-      in_bbox_filtertoken: "The data ​​shown in the plot are filtered according to the geometries/records selected and visible on the map",
-      relation: {
-        in_bbox: "The data​​ shown in the plot are filtered according to the reference (parents) geometries visible on the map",
-        filtertoken: "The data ​​shown in the plot are filtered according to the reference (parents) selected geometries/records",
-        in_bbox_filtertoken: "The data ​​shown in the plot are filtered according to the reference (parents) geometries/records selected and visible on the map"
-      }
-    }
-  },
-  fi: {
-    title: 'Charts',
-    pietitle: "Pie Charts",
-    no_subplots_charts: 'Exclusive plots',
-    no_plots : 'No chart. Add at least one plot',
-    no_data: "No Data available",
-    tooltip: {
-      show_all_features_on_map: "Filter all plots according to the geometries visible on the map",
-      show_feature_on_map: "Filter plot according to the geometries visible on the map",
-      filter_chart: "Filter plots according to the selected geometries/records"
-    },
-    filters: {
-      in_bbox: "The data ​​shown in the plot are filtered according to the geometries visible on the map",
-      filtertoken: "The data ​​shown in the plot are filtered according to the selected geometries/records",
-      in_bbox_filtertoken: "The data ​​shown in the plot are filtered according to the geometries/records selected and visible on the map",
-      relation: {
-        in_bbox: "The data​​ shown in the plot are filtered according to the reference (parents) geometries visible on the map",
-        filtertoken: "The data ​​shown in the plot are filtered according to the reference (parents) selected geometries/records",
-        in_bbox_filtertoken: "The data ​​shown in the plot are filtered according to the reference (parents) geometries/records selected and visible on the map"
-      }
-    }
-  },
-  se: {
-    title: 'Charts',
-    pietitle: "Pie Charts",
-    no_subplots_charts: 'Exclusive plots',
-    no_plots : 'No chart. Add at least one plot',
-    no_data: "No Data available",
-    tooltip: {
-      show_all_features_on_map: "Filter all plots according to the geometries visible on the map",
-      show_feature_on_map: "Filter plot according to the geometries visible on the map",
-      filter_chart: "Filter plots according to the selected geometries/records"
-    },
-    filters: {
-      in_bbox: "The data ​​shown in the plot are filtered according to the geometries visible on the map",
-      filtertoken: "The data ​​shown in the plot are filtered according to the selected geometries/records",
-      in_bbox_filtertoken: "The data ​​shown in the plot are filtered according to the geometries/records selected and visible on the map",
-      relation: {
-        in_bbox: "The data​​ shown in the plot are filtered according to the reference (parents) geometries visible on the map",
-        filtertoken: "The data ​​shown in the plot are filtered according to the reference (parents) selected geometries/records",
-        in_bbox_filtertoken: "The data ​​shown in the plot are filtered according to the reference (parents) geometries/records selected and visible on the map"
-      }
-    }
-  },
-  fr: {
-    title: 'Graphiques',
-    pietitle: "Cake Charts",
-    no_subplots_charts: 'Graphiques exclusifs',
-    no_plots : 'Pas de graphiques à montrer. Ajoutez au moins un graphique',
-    no_data: "Aucune donnée disponible",
-    tooltip: {
-      show_all_features_on_map: "Filtrer ce graphique en fonction des éléments visibles sur la carte",
-      show_feature_on_map: "Filtrer tous les graphiques en fonction des éléments sélectionnés",
-      filter_chart: "Filtrer tous les graphiques en fonction des éléments sélectionnés"
-    },
-    filters: {
-      in_bbox: "Les données présentées dans le graphique sont filtrées en fonction des géométries visibles sur la carte",
-      filtertoken: "Les données affichées dans le graphique sont filtrées en fonction des géométries/enregistrements sélectionnés",
-      in_bbox_filtertoken: "Les données présentées dans le graphique sont filtrées en fonction des géométries/enregistrements sélectionnés visibles sur la carte",
-      relation: {
-        in_bbox: "Les données présentées dans le graphique sont filtrées en fonction des géométries de référence (parents) visibles sur la carte",
-        filtertoken: "Les données affichées dans le graphique sont filtrées par les géométries/enregistrements de référence sélectionnés (parents)",
-        in_bbox_filtertoken: "Les données présentées dans le graphique sont filtrées en fonction des géométries/enregistrements de référence sélectionnés (parents) visibles sur la carte"
-      }
-    }
-  },
-  de: {
-    title: 'Diagramme',
-    pietitle: "Torten-Diagramme",
-    no_subplots_charts: 'Exklusive Diagramme',
-    no_plots : 'Kein Diagramm vorhanden. Mindestens ein Diagramm hinzufügen',
-    no_data: "Keine Daten verfügbar",
-    tooltip: {
-      show_all_features_on_map: "Alle Diagramme nach den auf der Karte sichtbaren Elementen filtern",
-      show_feature_on_map: "Dieses Diagramm nach den auf der Karte sichtbaren Elementen filtern",
-      filter_chart: "Diagramme nach ausgewählten Elementen filtern"
-    },
-    filters: {
-      in_bbox: "Die im Diagramm angezeigten Daten sind nach den auf der Karte sichtbaren Geometrien gefiltert",
-      filtertoken: "Die im Diagramm angezeigten Daten sind nach den ausgewählten Elementen gefiltert",
-      in_bbox_filtertoken: "Die im Diagramm angezeigten Daten sind nach den ausgewählten und auf der Karte sichtbaren Geometrien/Datensätzen gefiltert",
-      relation: {
-        in_bbox: "Die im Diagramm angezeigten Daten sind nach den auf der Karte sichtbaren Referenzgeometrien (Eltern) gefiltert",
-        filtertoken: "Die im Diagramm angezeigten Daten sind nach den ausgewählten Referenzgeometrien/-datensätzen (Eltern) gefiltert.",
-        in_bbox_filtertoken: "Die im Diagramm angezeigten Daten sind nach den ausgewählten und auf der Karte sichtbaren Referenzgeometrien/-datensätzen (Eltern) gefiltert"
-      }
-    }
-  },
-  ro: {
-    title: 'Grafice',
-    pietitle: "Diagrame radiale",
-    no_subplots_charts: 'Exclusiv grafice',
-    no_plots : 'Fără grafice. Adaugă unul',
-    no_data: "Nu avem date disponibile",
-    tooltip: {
-      show_all_features_on_map: "Filtrează toate graficele după geometriile vizibile în hartă",
-      show_feature_on_map: "Filtrează graficul cu geometriile vizibile în hartă",
-      filter_chart: "Filtrează graficele după entitățile selectate"
-    },
-    filters: {
-      in_bbox: "Datele ​​prezentate în grafic sunt filtrate după geometriile vizibile pe hartă",
-      filtertoken: "Datele ​​prezentate în grafic sunt filtrate după entitățile selectate",
-      in_bbox_filtertoken: "Datele ​​prezentate în grafic sunt filtrate după entitățile selectate și vizibile pe hartă",
-      relation: {
-        in_bbox: "Datele ​​ prezentate în grafic sunt filtrate după geometriile vizibile în hartă ale entităților relaționate de tip părinte",
-        filtertoken: "Datele ​​prezentate în grafic sunt filtrate după entitățile relaționate de tip părinte care sunt selectate",
-        in_bbox_filtertoken: "Datele ​​prezentate în grafic sunt filtrate după entitățile relaționate de tip părinte care sunt selectate și vizibile în hartă"
-      }
-    }
-  }
+  it,
+  en,
+  fi,
+  se,
+  fr,
+  de,
+  ro
 }
